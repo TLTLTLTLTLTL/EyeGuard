65777529625910dfea5abf6b582239fc127dfff3
TLTLTLTLTLTL <89323196+TLTLTLTLTLTL@users.noreply.github.com>
2024-10-03 23:30:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,8 +56,8 @@ with tab1:
 	    st.title("📊 Statistics on Cataracts")
 	    
 	    st.markdown("""<p class="big-font"><ul>
-            <li>Each year, around <b>24 million Americans aged 40 and older<b> are affected by cataracts, making it one of the most common eye conditions in the U.S.</li>
-	    <li>Over <b>3.7 million cataract surgeries<b> are performed annually.</li></ul></p>""", unsafe_allow_html=True)
+            <li class="big-font">Each year, around <b>24 million Americans aged 40 and older<b> are affected by cataracts, making it one of the most common eye conditions in the U.S.</li>
+	    <li class="big-font">Over <b>3.7 million cataract surgeries<b> are performed annually.</li></ul></p>""", unsafe_allow_html=True)
 	    
 	    # Create a sample dataframe for surgeries
 	    surgery_data = pd.DataFrame({
