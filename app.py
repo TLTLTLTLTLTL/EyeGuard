600002b22bd12191e44d260780cf7f21cd91e1c0
vestrol <abhinav.rathi@athenaeducation.co>
2024-10-09 15:48:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Cataract Scanner", layout="wide")
 st.markdown("""
     <style>
     .big-font {
-        font-size:18px !important;
+        font-size:16px !important;
     }
     </style>
     """, unsafe_allow_html=True)
